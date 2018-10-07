1cb4979a2f44c077ad24fd10fd59216f82a58f56
lightning1141 <lightning1141@gmail.com>
2018-10-07 14:49:42 +0800

    update linux version

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,27 @@
 extern crate log;
 extern crate log4rs;
 extern crate reqwest;
-#[macro_use]
-extern crate error_chain;
+extern crate failure;
 extern crate clap;
 extern crate regex;
 
-use log::LogLevelFilter;
+use failure::Error;
+use log::LevelFilter;
 use log4rs::append::file::FileAppender;
 use log4rs::encode::pattern::PatternEncoder;
 use log4rs::config::{Appender, Config, Root};
 use std::io::Read;
-use std::process::Command;
 use clap::{Arg, App};
 use regex::Regex;
-use reqwest::header::UserAgent;
+use reqwest::header::USER_AGENT;
 use std::thread;
 
-error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
-}
-
-fn search(word: &str) -> Result<()> {
+fn search(word: &str) -> Result<(), Error> {
     let user_agent = "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0";
     let url = format!("http://m.youdao.com/dict?le=eng&q={}", word);
-    let client = reqwest::Client::new()?;
-    let mut res = client.get(&url)?
-        .header(UserAgent::new(user_agent))
+    let client = reqwest::Client::new();
+    let mut res = client.get(&url)
+        .header(USER_AGENT, user_agent)
         .send()?;
     let mut body = String::new();
     res.read_to_string(&mut body)?;
@@ -48,12 +40,19 @@ fn search(word: &str) -> Result<()> {
     Ok(())
 }
 
+#[cfg(target_os="macos")]
 fn say(word: &str) {
+    use std::process::Command;
     let err = format!("Failed to say {:?}", word);
     Command::new("say")
             .arg(word)
             .output()
             .expect(&err);
+}
+
+#[cfg(not(target_os="macos"))]
+fn say(word: &str) {
+    println!("How to say {}?", word);
 }
 
 fn init_log() {
@@ -65,7 +64,7 @@ fn init_log() {
         .appender(Appender::builder().build("logfile", Box::new(logfile)))
         .build(Root::builder()
                    .appender("logfile")
-                   .build(LogLevelFilter::Info)).unwrap();
+                   .build(LevelFilter::Info)).unwrap();
 
     log4rs::init_config(config).unwrap();
 }
